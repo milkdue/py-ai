bf8b55fb1ade5285d6592791c9cb258a1fe4bc20
milkdue <321432896@qq.com>
2025-05-09 11:20:19 +0800

    feat(功能): ai摘要

--- api/index.py
+++ api/index.py
@@ -1,27 +1,35 @@
 import os
 from google import genai
 from http.server import BaseHTTPRequestHandler
+from google.genai import types
 import json
 import urllib.parse
-from urllib.parse import quote
+import httpx
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         api_key = os.environ.get("GOOGLE_API_KEY")
-        print(api_key)
         path = self.path
         query = path.split('?')[-1]
         params = urllib.parse.parse_qs(query)
-        content = params.get("content", [""])[0]
-        print(f"未解谜--content: {content}")
-        if content is not None:
-            content = quote(content, safe='')
-        print(f"解谜--content: {content}")
+        html_url = params.get("url", [""])[0]
+        client = genai.Client(api_key=api_key)
+        html_content = httpx.get(html_url).content
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=[
+                types.Part.from_bytes(
+                    data=html_content,
+                    mime_type="text/html",
+                ),
+                "根据网页内容生成一个摘要"
+            ]
+        )
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps({
             "id": 0,
-            "summary": content
+            "summary": response.text
         }).encode('utf-8'))
         return
